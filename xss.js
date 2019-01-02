14a6d11ad7f1865c6515c758ca8a0c2430fa14a5
EstebanRi <rivarolaesteban@hotmail.com>
2019-01-02 17:06:02 -0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,3 +1,4 @@
+(function(){
 var _aa = _dd[0].parentElement;
 
 _aa.removeChild(_dd[0]);
@@ -5,3 +6,4 @@ _aa.removeChild(_dd[0]);
 var __e = document.createElement("div");
 __e.innerHTML= '<center><iframe width="1280" height="745" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allow="accelerometer; autoplay=true; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>'
 _aa.append(__e);
+})();
